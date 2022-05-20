101ef44b4d4b23c9b008f045ba1dac7a99352623
Morten Bendiksen <morten.bendiksen@gmail.com>
2022-05-20 12:19:59 +0200

    first

---
+++ main.py
@@ -0,0 +1,7 @@
+
+def hi(name):
+    return f'Hi, {name}'
+
+
+if __name__ == '__main__':
+    print(hi('PyCharm'))
---
+++ test.py
@@ -0,0 +1,5 @@
+from main import hi
+
+
+def test_hi():
+    assert hi("foo") == "Hi, foo"
