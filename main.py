10f83e7efb2f4b4aa67360772ccf671b2c6d6f35
DanSava <dan.sava42@gmail.com>
2022-05-20 15:02:28 +0300

    X

--- main.py
+++ main.py
@@ -3,10 +3,39 @@ import math
 def roman(value):
     retval = ""
     if value % 500 == 0:
-        retval =  (value // 500) * "L"
+        retval += (value // 500) * "L"
+        value -= (value // 500) * 500
+    if value % 400 == 0:
+        retval += (value // 400) * "CL"
+        value -= (value // 400) * 400
     if value % 100 == 0:
-        retval +=  (value // 100) * "X"
-
+        retval += (value // 100) * "C"
+        value -= (value // 100) * 100
+    if value % 90 == 0:
+        retval += (value // 90) * "XC"
+        value -= (value // 90) * 90
+    if value % 50 == 0:
+        retval += (value // 50) * "L"
+        value -= (value // 50) * 50
+    if value % 40 == 0:
+        retval += (value // 40) * "XL"
+        value -= (value // 40) * 40
+    if value % 10 == 0:
+        retval += (value // 10) * "X"
+        value -= (value // 10) * 10
+    if value % 9 == 0:
+        retval += (value // 9) * "IX"
+        value -= (value // 9) * 9
+    if value % 5 == 0:
+        retval += (value // 5) * "V"
+        value -= (value // 5) * 5
+    if value % 4 == 0:
+        retval += (value // 4) * "IV"
+        value -= (value // 4) * 4
+    if value % 1 == 0:
+        retval += (value // 1) * "I"
+        value -= (value // 1) * 1
+    return retval
     if value < 100:
         if value == 40:
             return "XL"
