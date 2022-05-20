2655ac576437224250b7e2bbcab59f63bd57deb7
DanSava <dan.sava42@gmail.com>
2022-05-20 14:04:31 +0300

    IV

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 
 def roman(value):
+    roman_values = {
+        0: "",
+        1: "I",
+        10: "X",
+    }
     if value >= 10:
         return "X" + roman(value-10)
     elif value == 1:
@@ -8,3 +13,5 @@ def roman(value):
         return "X"
     elif value == 7:
         return "VII"
+    elif value == 0:
+        return ""
