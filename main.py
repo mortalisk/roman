ec3008e66e5549c8aa3361faa61218a11a6957f4
Øystein Olai Heggen <ooh@equinor.com>
2022-05-20 12:44:38 +0200

    Too complicated test

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 
 def roman(value):
-    if value == 1:
-        return f'I'
-    else:
+    if value >= 10:
+        return "X" + roman(value-10)
+    elif value == 1:
+        return 'I'
+    elif value == 10:
         return "X"
+    elif value == 7:
+        return "VII"
--- test.py
+++ test.py
@@ -8,5 +8,19 @@ def test_1():
 def test_10():
     assert roman(10) == "X"
 
+
 def test_7():
-    assert roman(7) ==
+    assert roman(7) == "VII"
+
+
+def test_bigger_than_10():
+    assert roman(11) == "XI"
+
+#def test_random():
+#    range_size = 5
+#
+#    romans = set()
+#    for i in range(1, range_size):
+#        romans.add(roman(i))
+#
+#    assert len(romans) == range_size
